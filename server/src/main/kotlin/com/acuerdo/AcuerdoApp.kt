87518afab7237a44037ebdf50cccd45ffe11e8c8
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2018-09-06 01:18:22 +0300

    Add Greeting

--- server/src/main/kotlin/com/acuerdo/AcuerdoApp.kt
+++ server/src/main/kotlin/com/acuerdo/AcuerdoApp.kt
@@ -1,4 +1,11 @@
 package com.acuerdo
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
+@SpringBootApplication
 class AcuerdoApp
+
+fun main(args: Array<String>) {
+    runApplication<AcuerdoApp>(*args)
+}
---
+++ server/src/main/kotlin/com/acuerdo/web/dto/Greeting.kt
@@ -0,0 +1,6 @@
+package com.acuerdo.web.dto
+
+data class Greeting(
+        val id: Long,
+        val content: String
+)
