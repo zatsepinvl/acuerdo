d60b0dc3b3cd66e87bf1775e01c2f6599e598c27
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2019-03-15 23:36:43 +0300

    Use gradle to build contracts

--- server/web3/generate.sh
+++ server/web3/generate.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #script directory
-targetPath=${dir}/../src/main/resources/contracts
-network=${1:-testrpc}
-contracts="Channels.json"
-mkdir -p ${targetPath}
+resourcesPath=${1:-${dir}/../build/generated-resources/contracts}
+sourcesPath=${2:-${dir}/../build/generated-sources}
+contracts=${3}
+network=${4:-testrpc}
+mkdir -p ${resourcesPath}
 for contract in ${contracts}
 do
-   cp  ${dir}/../../smart-contract/builds/${network}/${contract} ${targetPath}
-   ${dir}/web3j-3.6.0/bin/web3j truffle generate ${targetPath}/${contract} \
-                        -o ${dir}/../src/main/java \
+   cp  ${dir}/../../smart-contract/builds/${network}/${contract} ${resourcesPath}
+   ${dir}/web3j-3.6.0/bin/web3j truffle generate ${resourcesPath}/${contract} \
+                        -o ${sourcesPath} \
                         -p com.acuerdo.web3.contract
 done
